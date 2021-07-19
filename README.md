b1c09aed993bce0e9434ba97339357a480d43e58
caixarel <44868812+caixarel@users.noreply.github.com>
2021-07-19 16:33:09 +0200

    final

---
+++ README.md
@@ -0,0 +1,9 @@
+Static Node.js and Express Site
+
+In this project i will create a web site that will display my previous projects and also give some information about me.
+
+
+Changes made to original project:
+-Portfolio-intro's font family was changed. 
+-Personalized logo displayed on the top right side of the page.
+-When hovering over the project's images they will perform an animation. 
--- app.js
+++ app.js
@@ -1,13 +1,47 @@
+//The requirements for the project
 const express = require ("express");
 const {projects} = require("./data.json");
-
 const app =express();
 
+
 app.set('view engine','pug');
+//the files stored in the folder 'public' will be considered static files
 app.use('/static',express.static('public'));
 
+//the home page route
 app.get('/',(req,res)=>{
     res.render("index",{projects});
+})
+
+//the about page route
+app.get("/about",(req,res)=>{
+    res.render("about");
+})
+//the project page route
+app.get("/project/:id",(req,res)=>{
+    const id  = req.params.id;
+    //a new project page will be displayed for each project
+    const project = projects[id];
+    res.render("project",{project});
+})
+//error handle for non found pages
+app.use((req,res,next)=>{
+    const error = new Error('Page not found');
+    error.status = 404;
+    next(error);
+})
+//global error handler
+app.use((err,req,res,next)=>{
+    if (err.status== 404){
+        err.message = 'Error occured , page not found.'
+        res.render('page-not-found',{err});
+    }
+    else{
+        err.message = 'Error , return to main page '
+        err.status=500;
+        res.render('error',{err});
+    }
+   
 })
 
 app.listen(3000, () => {
